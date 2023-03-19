38a02ba6fb4b761e09c21d3a5ecf0b51e49cfd11
LichengXu99 <Lichengxu053@gmail.com>
2023-03-20 02:24:40 +0800

    feat: init repository, add User entity and config JWT

---
+++ spring-boot-backend/src/main/java/com/milestonecode/springboot/user/Role.java
@@ -0,0 +1,12 @@
+package com.milestonecode.springboot.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    ADMIN,
+    USER;
+
+}
